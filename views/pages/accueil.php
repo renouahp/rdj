6433151d176cbccfd371fa1f6647835c8877a783
Kira <grimfaith@users.noreply.github.com>
2020-12-01 15:30:04 +0100

    404.php v1

--- views/pages/accueil.php
+++ views/pages/accueil.php
@@ -1,11 +1,18 @@
-<div class="container-fluid">
+<div class="container-fluid mb-5">
     <div class="row">
         <div class="cover-accueil d-flex">
             <h1 class="align-self-center ml-auto mr-auto">Mega Drop Web.</h1>
         </div>
         <div class="col-12 text-center mt-5">
             <h3>Bienvenue sur notre site web.</h3>
-            <p>Consulter notre cataogue <a href="#">Ici</a>.</p>
+            <p>Consulter notre catalogue <a href="#">Ici</a>.</p>
+        </div>
+        <div class="col-12 d-flex gg justify-content-center">
+            <h4 class="align-self-center">Mega Drop Web pour des Méga promo</h4>
+        </div>
+        <div class="col-12 text-center mt-5">
+            <h4>Bonne visite</h4>
+            <p>Bonne visite, pensez à vous inscrire à notre newsletter <a href="index.php?page=4">Ici</a>.</p>
         </div>
     </div>
 </div>
