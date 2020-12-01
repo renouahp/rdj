3aee9890775a0c69810884656618be2a55d8cd4b
Kira <grimfaith@users.noreply.github.com>
2020-12-01 14:39:28 +0100

    acceuil update

--- views/pages/accueil.php
+++ views/pages/accueil.php
@@ -1,7 +1,10 @@
 <div class="container-fluid">
     <div class="row">
-        <div class="cover-accueil">
-        
+        <div class="cover-accueil d-flex">
+            <h1 class="align-self-center ml-auto mr-auto">Mega Drop Web.</h1>
+        </div>
+        <div class="col-12 text-center mt-5">
+            <h3>Bienvenue sur notre site web.</h3>
         </div>
     </div>
 </div>
