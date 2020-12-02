364e62c1eed52281537b87e222d40366e669a667
Kira <grimfaith@users.noreply.github.com>
2020-12-02 10:20:05 +0100

    v

--- views/templates/header.php
+++ views/templates/header.php
@@ -14,34 +14,38 @@
 <body>
     <div class="container-fluid">
         <div class="row">
-            <nav class="navbar navbar-expand-lg navbar-light sticky-top bg-light">
+            <nav class="col-12 navbar navbar-expand-lg navbar-light sticky-top bg-light">
                 <a class="navbar-brand" href="index.php">Mega Drop Web</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link <?php if($page == 'accueil' || $page == '') echo 'active' ?>" href="index.php">Accueil</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Produits</a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Vêtements</a>
-                        <a class="dropdown-item" href="#">Livres</a>
-                        <a class="dropdown-item" href="#"> Matériel informatique</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link <?php if($page == 'login') echo 'active' ?>" href="index.php?page=login">Connexion</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">S'enregistrer</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Panier</a>
-                    </li>
+                        <li class="nav-item active">
+                            <a class="nav-link <?php if($page == 'accueil' || $page == '') echo 'active' ?>" href="index.php">Accueil</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Produits</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Vêtements</a>
+                            <a class="dropdown-item" href="#">Livres</a>
+                            <a class="dropdown-item" href="#"> Matériel informatique</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link <?php if($page == 'login') echo 'active' ?>" href="index.php?page=login">Connexion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">S'enregistrer</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Panier</a>
+                        </li>
                     </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2 d-inline w-auto" type="search" placeholder="Pulls, livres, Pc..">
+                        <button class="btn btn-outline-success my-2 my-sm-0 position-relative ff" type="submit">Chercher</button>
+                    </form>
                 </div>
             </nav>
         </div>
