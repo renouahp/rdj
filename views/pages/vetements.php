629457ee39ba32a8bed8575aa08123f6d77585e1
Kira <grimfaith@users.noreply.github.com>
2020-12-02 10:30:33 +0100

    Update vetements.php

--- views/pages/vetements.php
+++ views/pages/vetements.php
@@ -6,15 +6,15 @@ $un = 'root';
 $pw = 'root';
 
 // Create connection
-$conn = new mysqli($hn, $db, $un, $pw);
+$conn = new mysqli($hn, $un, $pw, $db);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "SELECT nom, categorie, description, prix, image FROM shopy WHERE categorie = Pulls or categorie = Jupes or categorie = Robes";
+// $sql = "SELECT * from shopy"; // fonctionne
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
